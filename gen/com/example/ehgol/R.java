220fa554937024e78db84cb5f53131a1a2175fed
Bruno Oliveira <bdsdoliveira@gmail.com>
2013-04-16 18:55:41 -0300

    custom list; working import of json to list

--- gen/com/example/ehgol/R.java
+++ gen/com/example/ehgol/R.java
@@ -22,14 +22,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_menu_search_holo_dark=0x7f020001;
+        public static final int ic_menu_search_holo_light=0x7f020002;
+        public static final int list_item_box=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int games_button=0x7f080001;
-        public static final int games_text=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int comment_button=0x7f080000;
+        public static final int games_list=0x7f080003;
+        public static final int games_search=0x7f080001;
+        public static final int games_search_image=0x7f080002;
+        public static final int list_item_text=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_games=0x7f030000;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int list_item=0x7f030002;
     }
     public static final class menu {
         public static final int games=0x7f070000;
@@ -37,7 +45,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int games_button=0x7f050002;
+        public static final int comment_button=0x7f050003;
+        public static final int games_search=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/example/ehgol/GamesActivity.java
+++ src/com/example/ehgol/GamesActivity.java
@@ -9,15 +9,20 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,28 +32,21 @@ public class GamesActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_games);
+        setContentView(R.layout.activity_main);
 
-        Button b = (Button) findViewById(R.id.games_button);
-        b.setOnClickListener(new View.OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
-        		GetGamesTask g = new GetGamesTask();
-        		JSONArray games_json = null;
-        		TextView t = (TextView) findViewById(R.id.games_text);
-        		try {
-					games_json = new JSONArray(g.execute().get());
-        			t.setText(games_json.toString());
-        		} catch (Exception e) {
-        			e.printStackTrace();
-        		}
-        		//for(String info : games_json.names()) {
-//        			t.append(games_json.toString() + "...\n");
-        		//}
-        	}
-        });
+        ListView games_list = (ListView) findViewById(R.id.games_list);
+        
+        try {
+			GetGamesTask getGames = new GetGamesTask();
+			JSONArray json_array = new JSONArray(getGames.execute().get());
+			games_list.setAdapter(new GamesListAdapter(json_array, this));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
     }
-
+    
+    
     private class GetGamesTask extends AsyncTask<String, Void, String> {
     	private ProgressDialog p = new ProgressDialog(GamesActivity.this);
     	
@@ -85,6 +83,63 @@ public class GamesActivity extends Activity {
 			Toast.makeText(getApplicationContext(), "Games loaded.", Toast.LENGTH_SHORT).show();
 		}
     }
+    
+    
+    
+    public class GamesListAdapter extends BaseAdapter {
+    	private JSONArray json;
+    	Context c;
+    	
+    	GamesListAdapter(JSONArray json, Context c) {
+    		this.json = json;
+    		this.c = c;
+    	}
+    	
+		@Override
+		public int getCount() {
+			return json.length();
+		}
+		
+		@Override
+		public JSONObject getItem(int i) {
+			try {
+				return json.getJSONObject(i);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		@Override
+		public long getItemId(int i) {
+			return i;
+		}
+		
+		@Override
+		public View getView(int i, View view, ViewGroup parent) {
+			View v = view;
+			
+			if (v == null) {
+				LayoutInflater l = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = l.inflate(R.layout.list_item, null);
+			}
+
+			TextView t = (TextView) v.findViewById(R.id.list_item_text);
+			try {
+			t.setText(json.getJSONObject(i).getString("team1").toString() +
+					" vs " +
+					json.getJSONObject(i).getString("team2").toString() +
+					"\nLocal: " +
+					json.getJSONObject(i).getString("city").toString() +
+					", " +
+					json.getJSONObject(i).getString("stadium").toString());;
+			} catch (Exception e) {
+			}
+			return v;
+		}
+    }
+    
+    
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
