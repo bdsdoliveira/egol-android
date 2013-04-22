370e8074f02bee40e267dd951215a058b1f75f37
Bruno Oliveira <bdsdoliveira@gmail.com>
2013-04-21 23:07:53 -0300

    add share icons

--- gen/com/android/ehgol/R.java
+++ gen/com/android/ehgol/R.java
@@ -142,10 +142,12 @@ containing a value of this type.
         public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_menu_search_holo_dark=0x7f020001;
-        public static final int ic_menu_search_holo_light=0x7f020002;
-        public static final int item_box=0x7f020003;
+        public static final int facebook=0x7f020000;
+        public static final int gplus=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_menu_search_holo_dark=0x7f020003;
+        public static final int item_box=0x7f020004;
+        public static final int twitter=0x7f020005;
     }
     public static final class id {
         public static final int action_reload=0x7f040015;
